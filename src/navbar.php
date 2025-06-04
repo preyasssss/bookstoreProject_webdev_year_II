4f45a39fb3892f02ab5e09cb0cb22fc036a2bf18
Tincu Cosmin <cosmin@Tincus-MacBook-Pro.local>
2025-06-04 09:27:33 +0300

    update

--- src/navbar.php
+++ src/navbar.php
@@ -7,12 +7,12 @@ if (session_status() === PHP_SESSION_NONE) {
     <ul>
         <li><a href="index.php">Acasa</a></li>
         <li><a href="books.php">Carti</a></li>
-        <li><a href="login.php">Login</a></li>
+        <li><a href="login.php">Login/Admin</a></li>
         <li><a href="register.php">Register</a></li>
         <?php if (isset($_SESSION['user_id'])): ?>
-            <li><a href="admin.php">Admin</a></li>
+            <!--<li><a href="admin.php">Admin</a></li>-->
             <li><a href="add.php">Adauga o carte</a></li>
-            <li><a href="delete.php">Delete Book</a></li>
+            <!--<li><a href="delete.php">Delete Book</a></li>-->
             <li><a href="logout.php">Log out</a></li>
         <?php endif; ?>
     </ul>
